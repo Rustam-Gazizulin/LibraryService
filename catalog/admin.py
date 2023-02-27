88da902f318984aadf2cb91bdff37faf8f59fc84
rgazizulin174 <rgazizulin174@gmail.com>
2023-02-28 01:10:33 +0500

    добавил возможность добавлять автора

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Reader, Author, Books
 
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
 admin.site.register(Reader)
 admin.site.register(Author)
-admin.site.register(Books)
+admin.site.register(Books, BookAdmin)
 
--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from rest_framework import serializers
 
 from catalog.models import Author, Books, Reader
@@ -38,18 +39,35 @@ class BooksRetrieveUpdateDestroySerializer(serializers.ModelSerializer):
 
 class BooksCreateSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
+    authors = serializers.SlugRelatedField(
+        many=True,
+        required=False,
+        queryset=Author.objects.all(),
+        slug_field='lastname'
+    )
 
     class Meta:
         model = Books
-        exclude = ['authors']
+        fields = '__all__'
 
     def is_valid(self, *, raise_exception=False):
+        self._authors = self.initial_data.pop('authors')
         return super().is_valid(raise_exception=raise_exception)
 
     def create(self, validated_data):
         books = Books.objects.create(**validated_data)
+        try:
+            for author in self._authors:
+                author_obj = Author.objects.get(lastname=author)
+                books.authors.add(author_obj)
+
+        except Author.DoesNotExist:
+            raise serializers.ValidationError({"error": "Такого автора нет в каталоге, добавьте сначала автора "
+                                                        "потом книгу"})
         books.save()
         return books
+
+
 
 
 
