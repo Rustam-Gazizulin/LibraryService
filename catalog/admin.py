4c93bd4951c40d5873734c8ce23a7b93dc53ccca
rgazizulin174 <rgazizulin174@gmail.com>
2023-02-28 18:39:45 +0500

    кастомизировал админку

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,52 @@
 from django.contrib import admin
+from django.db.models import QuerySet
+from django.utils.html import format_html
+from django.urls import reverse
+
 from .models import Reader, Author, Books
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('name', 'description', 'number_of_books', 'authors_link')
+    search_fields = ('name',)
+    actions = ['zero_nums_books']
+
+    def authors_link(self, obj):
+        author = obj.authors
+        url = reverse("admin:catalog_author_changelist") + str(author.pk)
+        return format_html(f'<a href="{url}"> {author} </a>')
+
+    authors_link.short_description = 'Авторы'
+
+    @admin.action(description='Нет в наличии')
+    def zero_nums_books(self, request, queryset: QuerySet):
+        queryset.update(number_of_books=0)
+        self.message_user(request, f'Книг нет в наличии')
 
 
-admin.site.register(Reader)
-admin.site.register(Author)
+class ReaderAdmin(admin.ModelAdmin):
+    list_display = ('lastname', 'phone', 'status_reader', 'display_books')
+    list_filter = ('status_reader',)
+    actions = ['change_status_reader']
+
+    @admin.action(description='Изменить статус читателя')
+    def change_status_reader(self, request, queryset: QuerySet):
+        count = queryset.update(status_reader=False)
+        self.message_user(request, f'Заблокировано {count} читателей')
+
+    # @admin.action(description='Удалить книги у читателя')
+    # def zero_active_books(self, request, queryset: QuerySet):
+    #     books = Books.objects.all()
+    #     for book in books:
+    #         queryset.delete(Reader.active_books[book])
+    #     self.message_user(request, f'У читателей нет книг')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'date_of_created')
+
+
+admin.site.register(Reader, ReaderAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Books, BookAdmin)
 
--- catalog/models.py
+++ catalog/models.py
@@ -14,17 +14,17 @@ class Author(models.Model):
         ordering = ['lastname']
 
     def __str__(self):
-        return self.firstname
+        return f'{self.firstname} {self.lastname}'
 
 
 class Books(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Название')
+    name = models.CharField(max_length=50, verbose_name='Название', unique=True)
     description = models.TextField(max_length=2000, verbose_name='Описание')
     number_of_pages = models.PositiveIntegerField(verbose_name='Количество страниц')
     number_of_books = models.PositiveIntegerField(verbose_name='Количество книг')
     date_of_created = models.DateField(auto_now_add=True, editable=True, verbose_name='Дата создания')
     date_edited = models.DateField(auto_now=True, verbose_name='Дата редактирования')
-    authors = models.ManyToManyField(Author, related_name='author', blank=True)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True)
 
     class Meta:
         verbose_name = 'Книга'
@@ -38,16 +38,20 @@ class Books(models.Model):
 class Reader(models.Model):
     firstname = models.CharField(max_length=30, verbose_name='Имя читателя')
     lastname = models.CharField(max_length=30, verbose_name='Фамилия читателя')
-    phone = models.BigIntegerField(verbose_name='Номер телефона')
-    status_reader = models.BooleanField(default=True)
+    phone = models.BigIntegerField(verbose_name='Номер телефона', unique=True)
+    status_reader = models.BooleanField(default=True, verbose_name="Статус")
     active_books = models.ManyToManyField(Books, related_name='books', blank=True)
 
     class Meta:
         verbose_name = 'Читатель'
         verbose_name_plural = 'Читатели'
 
+    def display_books(self):
+        return ' | '.join([active_books.name for active_books in self.active_books.all()])
+    display_books.short_description = 'Книги на руках'
+
     def __str__(self):
-        return self.firstname
+        return f'{self.firstname} {self.lastname}'
 
 
 
